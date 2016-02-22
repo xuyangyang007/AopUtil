92506a5acdf4722ffff2ef792d522a32d2bb896b
yangyang.xu <378032058@qq.com>
2016-02-22 23:51:27 +0800

    add advice

---
+++ cache-aop/src/main/java/com/cache/aop/advice/CommonAdvice.java
@@ -0,0 +1,49 @@
+package com.cache.aop.advice;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * 
+ * @since 1.7
+ * @author yangyang.xu
+ *
+ */
+public abstract class CommonAdvice<T> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CommonAdvice.class);
+    
+    protected Logger getLogger() {
+        return LOG;
+    }
+    
+    public Method getMethod(final JoinPoint jp) throws Exception {
+        if (jp == null) {
+            throw new Exception("JoinPoint is null");
+        }
+        final Signature sig = jp.getSignature();
+        if (!(sig instanceof MethodSignature)) {
+            throw new NoSuchMethodException("NoSuchMethodException");
+        }
+        MethodSignature methodSignature = (MethodSignature) sig;  
+        Method method = methodSignature.getMethod();  
+        return method;
+    }
+    
+    public Class getTargetClass(final JoinPoint jp) throws Exception {
+        if (jp == null) {
+            throw new Exception("JoinPoint is null");
+        }
+        return jp.getTarget().getClass();
+    }
+    
+    protected abstract T getAnnotationData(ProceedingJoinPoint pjp);
+    
+}
