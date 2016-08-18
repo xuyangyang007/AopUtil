e6abccbb096d33960f818ef7078ebb0a5c4a3c94
yangyang.xu <378032058@qq.com>
2016-08-18 14:07:21 +0800

    新增mc处理

---
+++ cache-aop/src/main/java/com/cache/handler/CacheBasicService.java
@@ -0,0 +1,12 @@
+package com.cache.handler;
+
+/**
+ * 缓存基础服务
+ * @author yangyang.xu
+ *
+ */
+public interface CacheBasicService {
+    
+    public void get();
+
+}
---
+++ cache-aop/src/main/java/com/cache/handler/impl/XmcServiceImpl.java
@@ -0,0 +1,10 @@
+package com.cache.handler.impl;
+
+/**
+ * xmemcache缓存实现
+ * @author yangyang.xu
+ *
+ */
+public class XmcServiceImpl {
+
+}
